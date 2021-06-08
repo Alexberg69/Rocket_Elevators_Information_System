6616a4508b8a91f2119d2b9e88afdbf10896ab2f
Alex Bergeron <alexbb_69@hotmail.com>
2021-06-08 17:10:02 -0400

    v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'website/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'website#index'
+  get '/index', to: 'website#index'
+
+  get '/residential', to: 'website#residential'
+
+  get '/commercial', to: 'website#commercial'
+
+  get '/quote', to: 'website#quote'
 end
