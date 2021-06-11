6388508656f4f58f895cdabb5da7ad06448d2870
Nathan Sheehy <nimolimolo@gmail.com>
2021-06-11 19:00:12 -0400

    RocketNS v2

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_06_08_174805) do
+ActiveRecord::Schema.define(version: 2021_06_09_200007) do
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -19,16 +18,37 @@ ActiveRecord::Schema.define(version: 2021_06_08_174805) do
     t.string "first_name"
     t.string "title"
   end
+
   create_table "quotes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "product_line"
-    t.int "number_of_elevator"
-    t.int "unit_price_of_each_elevator"
-    t.int "total_price_of_elevators"
-    t.int "installation_fees"
-    t.int "final_price"
+    t.decimal "number_of_appartement", precision: 10
+    t.decimal "number_of_floor", precision: 10
+    t.decimal "number_of_basement", precision: 10
+    t.decimal "number_of_companies", precision: 10
+    t.decimal "number_of_parking_spots", precision: 10
+    t.decimal "number_of_elevators", precision: 10
+    t.decimal "number_of_corporations", precision: 10
+    t.decimal "maximum_occupancy", precision: 10
+    t.decimal "business_hours", precision: 10
+    t.string "unit_price_of_each_elevator"
+    t.string "total_price_of_elevators"
+    t.string "installation_fees"
+    t.string "final_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
