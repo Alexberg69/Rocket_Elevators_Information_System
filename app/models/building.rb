77030b89b98e83af4dbbf093e6abea05c8c67c8a
Alex Bergeron <alexbb_69@hotmail.com>
2021-06-17 12:57:37 -0400

    update

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,5 +2,5 @@ class Building < ApplicationRecord
     belongs_to :address_of_the_building, class_name: "Address"
     belongs_to :customer, class_name: "Customer"
     has_one :building_detail
-    has_many :battery 
+    has_one :battery 
 end
